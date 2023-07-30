19f5f376285682c3af85ec5003f7aed17e6aba72
Johan Ronsse <johan.ronsse@gmail.com>
2023-07-29 19:24:44 -0600

    v1

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,12 +1,1 @@
 // Reexport your entry components here
-
-// How to handle the exports?
-export { default as Button } from './components/Button';
-
-export { default as Input } from '$lib/Input';
-export { default as Select } from '$lib/Select';
-export { default as Textarea } from '$lib/Textarea';
-export { default as Checkbox } from '$lib/Checkbox';
-export { default as Radio } from '$lib/Radio';
-export { default as Loading } from '$lib/Loading';
-
